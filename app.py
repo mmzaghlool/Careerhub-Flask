344f4c73024234cc18c03c8011dbf8c8d2c3c4b7
Mostafa Mahmoud <mmzaghlool52@gmail.com>
2020-04-02 21:11:53 +0200

    allow cors

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 import firebase_admin
 from firebase_admin import credentials, firestore, auth, db
 # from "./src/auth" import auth
 app = Flask(__name__)
+CORS(app)
 
 from routes import *
 app.register_blueprint(routes)
