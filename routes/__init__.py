77e3d5625bfea3b37629acb6c7dc41ec8ce543a3
Mostafa Mahmoud <mmzaghlool52@gmail.com>
2020-07-20 19:52:00 +0200

    init

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,14 +1,14 @@
 from flask import Blueprint
 routes = Blueprint('routes', __name__)
 
-from .index import *
+# from .index import *
 from .user import *
+from .ElasticSearch import *
+from .Search import *
 from .Chat import *
 from .Courses import *
 from .Groups import *
 from .Rs import *
 from .Notifications import *
 from .chatbot import *
-from .Search import *
-from .ElasticSearch import *
 #
